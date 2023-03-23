d64432d7dd20d3155ff48bacd6152afc139de375
Rishabh_Sharma_Projects <rishabhxyzkumar@gmail.com>
2023-03-23 17:03:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/rishabhsharma1212/scrapitems/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rishabhsharma1212.scrapitems
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
